7af04db0e2c0b44900bcc66ce76e1f8bd5e18b66
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-04-09 20:24:05 +0530

    Implement order creation endpoint using Fastify, TypeScript, and Prisma with authentication middleware

--- src/modules/order/order.routes.ts
+++ src/modules/order/order.routes.ts
@@ -1,10 +1,10 @@
 import { OrderController } from './order.controller';
-import { verifyToken } from '../../middleware/auth.middleware';
+import { authMiddleware } from '../../middleware/auth.middleware';
 
 export default async function orderRoutes(fastify: any) {
   const controller = new OrderController();
 
   fastify.get('/orders', controller.getAll.bind(controller));
   fastify.get('/orders/:id', controller.getById.bind(controller));
-  fastify.post('/orders', { preHandler: [verifyToken] }, controller.create.bind(controller));
+  fastify.post('/orders', { preHandler: authMiddleware },controller.create.bind(controller));
 }
