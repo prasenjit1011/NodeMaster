d5f1827d6ddb5f8d26a4db0e881cca47f829f185
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-04-11 16:31:29 +0530

    Type main branch setup

--- app.ts
+++ app.ts
@@ -1,18 +1,19 @@
+import { NextFunction } from "express";
+import express, { Request, Response } from "express";
 console.clear();
 console.log('\n\n-: App Started :-');
 
-const express   = require('express');
 const app       = express();
 
-app.use('/', (req, res, next)=>{
+app.use('/', (req:Request, res:Response, next:NextFunction)=>{
     console.log('-: Welcome :-');
-    res.send('-: Welcome :-');
+    res.status(200).send('-: Welcome :-');
 
     next()
 });
 
 // Centralized Error Handler
-app.use((err, req, res, next) => {
+app.use((err:Error, req:Request, res:Response, next:NextFunction) => {
     console.error('Central Error Handler:', err.message);
     res.status(500).json({ message: 'Internal Server Error', error: err.message });
 });
