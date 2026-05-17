5c9820b2f5b379a0335e3079f007c19712af42e6
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-17 11:49:50 +0530

    Variable updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,93 @@
-variable "project_id" {}
-variable "region" {}
-variable "zone" {}
+# -----------------------------
+# GCP Project Configuration
+# -----------------------------
+variable "project_id" {
+  description = "Google Cloud Project ID"
+  type        = string
+}
 
-variable "credentials_file" {}
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "asia-south1"
+}
 
-variable "vm_name" {}
-variable "machine_type" {}
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "asia-south1-a"
+}
 
-variable "image" {}
-variable "boot_disk_size" {}
+# -----------------------------
+# VM Configuration
+# -----------------------------
+variable "vm_name" {
+  description = "Name of Compute Engine VM"
+  type        = string
+  default     = "nodejs-vm"
+}
 
-variable "github_repo" {}
+variable "machine_type" {
+  description = "GCP VM machine type"
+  type        = string
+  default     = "e2-micro"
+}
+
+variable "image" {
+  description = "Boot disk image"
+  type        = string
+  default     = "debian-cloud/debian-12"
+}
+
+variable "boot_disk_size" {
+  description = "Boot disk size in GB"
+  type        = number
+  default     = 10
+}
+
+# -----------------------------
+# GitHub Repository
+# -----------------------------
+variable "github_repo" {
+  description = "GitHub repository URL"
+  type        = string
+}
+
+# -----------------------------
+# Network Configuration
+# -----------------------------
+variable "network_name" {
+  description = "VPC network name"
+  type        = string
+  default     = "default"
+}
+
+variable "subnetwork_name" {
+  description = "Subnetwork name"
+  type        = string
+  default     = "default"
+}
+
+# -----------------------------
+# Firewall Configuration
+# -----------------------------
+variable "allowed_ports" {
+  description = "List of allowed firewall ports"
+  type        = list(string)
+
+  default = [
+    "22",
+    "80",
+    "443",
+    "3000"
+  ]
+}
+
+# -----------------------------
+# Auto Delete
+# -----------------------------
+variable "enable_auto_delete" {
+  description = "Enable VM auto deletion"
+  type        = bool
+  default     = false
+}
