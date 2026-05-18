fd19f3af573a79a19f60f31fc0b2ac6ce1ca2cfa
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-18 22:24:27 +0530

    Load Balancer 1006

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,106 +1,65 @@
-resource "random_id" "vm_suffix" {
-  byte_length = 3
+# -----------------------------
+# 1. Load Balancer (Top Section)
+# -----------------------------
+
+resource "google_compute_global_address" "lb_ip" {
+  name = "lb-ip"
 }
 
-resource "google_compute_instance" "nodejs_vm" {
-  name         = "${var.vm_name}-${random_id.vm_suffix.hex}"
-  machine_type = var.machine_type
-  zone         = var.zone
+resource "google_compute_backend_service" "backend" {
+  name                  = "nodejs-backend"
+  protocol              = "HTTP"
+  load_balancing_scheme = "EXTERNAL"
+  port_name             = "http"
+  timeout_sec           = 30
 
-  boot_disk {
-    initialize_params {
-      image = var.image
-      size  = var.boot_disk_size
-    }
+  backend {
+    group = google_compute_instance_group_manager.app_group.instance_group
+  }
+}
+
+resource "google_compute_url_map" "url_map" {
+  name            = "url-map"
+  default_service = google_compute_backend_service.backend.self_link
+}
+
+resource "google_compute_target_http_proxy" "http_proxy" {
+  name    = "http-proxy"
+  url_map = google_compute_url_map.url_map.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "forwarding_rule" {
+  name       = "http-forwarding-rule"
+  target     = google_compute_target_http_proxy.http_proxy.self_link
+  port_range = "80"
+  ip_address = google_compute_global_address.lb_ip.address
+}
+
+# -----------------------------
+# 2. VM / Instance Group (Bottom Section)
+# -----------------------------
+
+resource "google_compute_instance_template" "app_template" {
+  name         = "app-template"
+  machine_type = "e2-medium"
+
+  disk {
+    boot = true
+    source_image = "debian-cloud/debian-12"
   }
 
   network_interface {
-    network = var.network_name
-
+    network = "default"
     access_config {}
   }
+}
 
-  metadata = {
-    MONGO_URI = var.mongo_uri
+resource "google_compute_instance_group_manager" "app_group" {
+  name               = "app-group"
+  base_instance_name = "app"
+  zone               = "us-central1-a"
+
+  version {
+    instance_template = google_compute_instance_template.app_template.id
   }
-
-  metadata_startup_script = file("${path.module}/startup.sh")
-
-  tags = ["nodejs"]
-
-  allow_stopping_for_update = true
-}
-
-resource "google_compute_firewall" "allow_http" {
-  name    = "${var.project_id}-allow-http"
-  network = var.network_name
-
-  allow {
-    protocol = "tcp"
-    ports    = var.allowed_ports
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["nodejs"]
-}
-
-resource "google_compute_instance_group" "nodejs_group" {
-  name    = "${var.vm_name}-group"
-  zone    = var.zone
-  network = var.network_name
-
-  instances = [google_compute_instance.nodejs_vm.self_link]
-
-  named_port {
-    name = "http"
-    port = var.lb_port
-  }
-}
-
-resource "google_compute_health_check" "http" {
-  name                = "${var.project_id}-http-health-check"
-  check_interval_sec  = 10
-  timeout_sec         = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 2
-
-  http_health_check {
-    request_path = "/"
-    port         = var.lb_port
-  }
-}
-
-resource "google_compute_backend_service" "nodejs_backend" {
-  name                  = "${var.project_id}-backend-service"
-  protocol              = "HTTP"
-  port_name             = "http"
-  timeout_sec           = 10
-  health_checks         = [google_compute_health_check.http.self_link]
-  load_balancing_scheme = "EXTERNAL"
-
-  backend {
-    group = google_compute_instance_group.nodejs_group.self_link
-  }
-}
-
-resource "google_compute_url_map" "nodejs_url_map" {
-  name            = "${var.project_id}-url-map"
-  default_service = google_compute_backend_service.nodejs_backend.self_link
-}
-
-resource "google_compute_target_http_proxy" "nodejs_proxy" {
-  name    = "${var.project_id}-http-proxy"
-  url_map = google_compute_url_map.nodejs_url_map.self_link
-}
-
-resource "google_compute_global_address" "lb_ip" {
-  name = "${var.project_id}-lb-ip"
-}
-resource "google_compute_global_forwarding_rule" "http" {
-  name                  = "${var.project_id}-http-forwarding-rule"
-  target                = google_compute_target_http_proxy.nodejs_proxy.self_link
-  port_range            = "${var.lb_port}"
-  load_balancing_scheme = "EXTERNAL"
-  ip_protocol           = "TCP"
-  ip_address            = google_compute_global_address.lb_ip.address
 }
