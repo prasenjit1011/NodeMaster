e7acca83f617767a649fe557d8c6e965457a4f56
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-23 06:35:22 +0530

    AWS-635

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
 }
 
 resource "aws_security_group" "node_sg" {
@@ -25,47 +25,27 @@ resource "aws_security_group" "node_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_instance" "node_server" {
-  ami           = "ami-03f4878755434977f"
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.node_sg.id]
+  key_name = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.node_sg.id
+  ]
 
   root_block_device {
     volume_size           = 8
     volume_type           = "gp3"
     delete_on_termination = true
   }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
-              yum install -y nodejs git
-
-              mkdir -p /home/ec2-user/app
-              cd /home/ec2-user/app
-
-              cat > server.js <<'EOL'
-              const express = require("express");
-              const app = express();
-
-              app.get("/", (req, res) => {
-                res.send("Hello from GitHub Actions");
-              });
-
-              app.listen(3000, "0.0.0.0", () => {
-                console.log("Running");
-              });
-              EOL
-
-              npm init -y
-              npm install express
-
-              nohup node server.js > app.log 2>&1 &
-              EOF
 
   tags = {
     Name = "github-actions-node"
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+# terraform/variables.tf
+
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-03f4878755434977f"
+}
+
+variable "app_port" {
+  default = 3000
+}
