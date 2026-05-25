d48cfceb5a7e1d6185607a50827cc477c765ff81
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-25 19:46:12 +0530

    AWS-K8

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,16 +17,17 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
+  version = "21.22.0"
 
-  cluster_name    = "node-cluster"
-  cluster_version = "1.30"
+  name               = "node-cluster"
+  kubernetes_version = "1.30"
 
-  subnet_ids = module.vpc.public_subnets
   vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.small"]
+      instance_types = ["t3.micro"]
 
       min_size     = 1
       max_size     = 1
