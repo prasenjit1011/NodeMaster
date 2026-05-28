5cb37d98d59daf63230093eb13ef4c86f2d35394
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-28 21:58:49 +0530

    c-958

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,8 @@ COPY package*.json ./
 
 RUN npm install
 
-COPY . .
+COPY tsconfig.json ./
+COPY src ./src
 
 RUN npm run build
 
--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,16 +1,13 @@
 resource "aws_lambda_function" "app" {
+
   function_name = "nodemaster"
 
   package_type = "Image"
 
-  image_uri = "${aws_ecr_repository.repo.repository_url}:latest"
+  image_uri = "${aws_ecr_repository.app.repository_url}:latest"
 
   role = aws_iam_role.lambda_role.arn
 
-  timeout = 30
+  timeout    = 30
   memory_size = 512
-}
-
-resource "aws_ecr_repository" "repo" {
-  name = "nodemaster"
 }
