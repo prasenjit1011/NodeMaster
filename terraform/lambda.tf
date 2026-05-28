7835a4fc5e624abc47436872261ac18ac7b2cd0a
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-28 09:10:20 +0530

    StepFn-910

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,19 +1,41 @@
-resource "aws_s3_bucket" "employee_bucket" {
-  bucket = "employee-system-${random_id.bucket_id.hex}"
-}
+##################################################
+# RANDOM ID FOR UNIQUE S3 BUCKET
+##################################################
 
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
-resource "aws_lambda_function" "login" {
-  function_name = "loginLambda"
-  role          = aws_iam_role.lambda_role.arn
-  runtime       = "nodejs20.x"
-  handler       = "index.handler"
-  filename      = "../login.zip"
+##################################################
+# S3 BUCKET
+##################################################
 
+resource "aws_s3_bucket" "employee_bucket" {
+
+  bucket = "employee-system-${random_id.bucket_id.hex}"
+
+  tags = {
+    Name = "employee-system-bucket"
+  }
+}
+
+##################################################
+# LOGIN LAMBDA
+##################################################
+
+resource "aws_lambda_function" "login" {
+
+  function_name = "loginLambda"
+
+  role    = aws_iam_role.lambda_role.arn
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+
+  filename         = "../login.zip"
   source_code_hash = filebase64sha256("../login.zip")
+
+  timeout     = 30
+  memory_size = 256
 
   environment {
     variables = {
@@ -21,48 +43,99 @@ resource "aws_lambda_function" "login" {
       MONGO_URL  = var.mongo_url
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic
+  ]
+
+  tags = {
+    Name = "login-lambda"
+  }
 }
 
-resource "aws_lambda_function" "validateJwt" {
-  function_name = "validateJwtLambda"
-  role          = aws_iam_role.lambda_role.arn
-  runtime       = "nodejs20.x"
-  handler       = "index.handler"
-  filename      = "../validateJwt.zip"
+##################################################
+# VALIDATE JWT LAMBDA
+##################################################
 
+resource "aws_lambda_function" "validateJwt" {
+
+  function_name = "validateJwtLambda"
+
+  role    = aws_iam_role.lambda_role.arn
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+
+  filename         = "../validateJwt.zip"
   source_code_hash = filebase64sha256("../validateJwt.zip")
+
+  timeout     = 15
+  memory_size = 128
 
   environment {
     variables = {
       JWT_SECRET = var.jwt_secret
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic
+  ]
+
+  tags = {
+    Name = "validate-jwt-lambda"
+  }
 }
 
-resource "aws_lambda_function" "createEmployee" {
-  function_name = "createEmployeeLambda"
-  role          = aws_iam_role.lambda_role.arn
-  runtime       = "nodejs20.x"
-  handler       = "index.handler"
-  filename      = "../createEmployee.zip"
+##################################################
+# CREATE EMPLOYEE LAMBDA
+##################################################
 
+resource "aws_lambda_function" "createEmployee" {
+
+  function_name = "createEmployeeLambda"
+
+  role    = aws_iam_role.lambda_role.arn
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+
+  filename         = "../createEmployee.zip"
   source_code_hash = filebase64sha256("../createEmployee.zip")
+
+  timeout     = 30
+  memory_size = 256
 
   environment {
     variables = {
       MONGO_URL = var.mongo_url
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic
+  ]
+
+  tags = {
+    Name = "create-employee-lambda"
+  }
 }
 
-resource "aws_lambda_function" "uploadImage" {
-  function_name = "uploadImageLambda"
-  role          = aws_iam_role.lambda_role.arn
-  runtime       = "nodejs20.x"
-  handler       = "index.handler"
-  filename      = "../uploadImage.zip"
+##################################################
+# UPLOAD IMAGE LAMBDA
+##################################################
 
+resource "aws_lambda_function" "uploadImage" {
+
+  function_name = "uploadImageLambda"
+
+  role    = aws_iam_role.lambda_role.arn
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+
+  filename         = "../uploadImage.zip"
   source_code_hash = filebase64sha256("../uploadImage.zip")
+
+  timeout     = 60
+  memory_size = 512
 
   environment {
     variables = {
@@ -70,20 +143,46 @@ resource "aws_lambda_function" "uploadImage" {
       JWT_SECRET  = var.jwt_secret
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic,
+    aws_iam_role_policy_attachment.lambda_s3
+  ]
+
+  tags = {
+    Name = "upload-image-lambda"
+  }
 }
 
-resource "aws_lambda_function" "updateEmployee" {
-  function_name = "updateEmployeeLambda"
-  role          = aws_iam_role.lambda_role.arn
-  runtime       = "nodejs20.x"
-  handler       = "index.handler"
-  filename      = "../updateEmployee.zip"
+##################################################
+# UPDATE EMPLOYEE LAMBDA
+##################################################
 
+resource "aws_lambda_function" "updateEmployee" {
+
+  function_name = "updateEmployeeLambda"
+
+  role    = aws_iam_role.lambda_role.arn
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+
+  filename         = "../updateEmployee.zip"
   source_code_hash = filebase64sha256("../updateEmployee.zip")
+
+  timeout     = 30
+  memory_size = 256
 
   environment {
     variables = {
       MONGO_URL = var.mongo_url
     }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic
+  ]
+
+  tags = {
+    Name = "update-employee-lambda"
   }
 }
