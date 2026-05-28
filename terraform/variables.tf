630507309fdde6acc6eb58ea9a4a323a5d8efeee
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-28 08:34:02 +0530

    AWS-StepFn-833

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "aws_region" {
 }
 
 variable "mongo_url" {
-  default = "mongodb+srv://myuser:password@cluster0.hlicuim.mongodb.net/demodb?retryWrites=true&w=majority&appName=Cluster0"
+  type = string
 }
 
 variable "jwt_secret" {
