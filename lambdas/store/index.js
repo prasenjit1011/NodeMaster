bdd27a4051f26052eb943e490e9a2195b96bb927
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-30 23:12:51 +0530

    Final

--- lambdas/store/index.js
+++ lambdas/store/index.js
@@ -19,7 +19,7 @@ exports.handler = async (event) => {
 
     return {
       found: true,
-      answer: "FAQ ANSWER : " + event.answer,
+      answer: "FAQ ANSWER MDB : " + event.answer,
       id: result.insertedId.toString()
     };
   } catch (err) {
