fc261f16405caddd8ebd7373aeff312755469fa1
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-30 09:48:42 +0530

    stb-948

--- infra/main.tf
+++ infra/main.tf
@@ -261,3 +261,31 @@ resource "aws_apigatewayv2_stage" "default" {
 output "api_gateway_url" {
   value = aws_apigatewayv2_stage.default.invoke_url
 }
+
+resource "aws_apigatewayv2_integration" "validate_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.validate.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "validate_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /validate"
+  target    = "integrations/${aws_apigatewayv2_integration.validate_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.validate.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*/POST/validate"
+}
