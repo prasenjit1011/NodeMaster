2f440776e8149f41538241aa39e6056cbcb45407
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-30 10:00:27 +0530

    stc-01

--- infra/main.tf
+++ infra/main.tf
@@ -284,3 +284,53 @@ resource "aws_lambda_permission" "apigw" {
 
   source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*/POST/validate"
 }
+
+resource "aws_apigatewayv2_integration" "stepfn_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_subtype = "StepFunctions-StartExecution"
+
+  credentials_arn = aws_iam_role.api_gateway_stepfn_role.arn
+
+  request_parameters = {
+    "StateMachineArn" = aws_sfn_state_machine.faq.arn
+  }
+}
+
+resource "aws_iam_role" "api_gateway_stepfn_role" {
+  name = "${var.project_name}-${var.environment}-apigw-stepfn-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "api_gateway_stepfn_policy" {
+  role = aws_iam_role.api_gateway_stepfn_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "states:StartExecution"
+      ]
+      Resource = aws_sfn_state_machine.faq.arn
+    }]
+  })
+}
+
+resource "aws_apigatewayv2_route" "faq_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /faq"
+
+  target = "integrations/${aws_apigatewayv2_integration.stepfn_integration.id}"
+}
+
