ac00cc1d98a04bb62526641903791654615fc83d
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-02 06:54:52 +0530

    5555

--- lambdas/createLeave/index.js
+++ lambdas/createLeave/index.js
@@ -6,8 +6,36 @@ exports.handler = async (event) => {
   
 
   try {
+    console.log("Create Leave Event:", JSON.stringify(event));
+    console.log("Mongo URI:", process.env.MONGO_URI);
+    
+    client = new MongoClient(process.env.MONGO_URI);
+
+    await client.connect();
+
+    const db = client.db("hrdb");
+    const collection = db.collection("leave_requests");
+
+    const leaveRequest = {
+      employeeId: event.employeeId,
+      employeeName: event.employeeName,
+      leaveType: event.leaveType,
+      fromDate: event.fromDate,
+      toDate: event.toDate,
+      reason: event.reason,
+
+      status: "PENDING_MANAGER",
+
+      managerApproved: false,
+      hrApproved: false,
+
+      createdAt: new Date()
+    };
+
+    const result = await collection.insertOne(leaveRequest);
+
     return {
-      leaveId: 44445,
+      leaveId: result.insertedId.toString(),
 
       employeeId: event.employeeId,
       employeeName: event.employeeName,
@@ -17,46 +45,7 @@ exports.handler = async (event) => {
       reason: event.reason,
 
       status: "PENDING_MANAGER"
-    };    
-
-
-    // client = new MongoClient(process.env.MONGO_URI);
-
-    // await client.connect();
-
-    // const db = client.db("hrdb");
-    // const collection = db.collection("leave_requests");
-
-    // const leaveRequest = {
-    //   employeeId: event.employeeId,
-    //   employeeName: event.employeeName,
-    //   leaveType: event.leaveType,
-    //   fromDate: event.fromDate,
-    //   toDate: event.toDate,
-    //   reason: event.reason,
-
-    //   status: "PENDING_MANAGER",
-
-    //   managerApproved: false,
-    //   hrApproved: false,
-
-    //   createdAt: new Date()
-    // };
-
-    // const result = await collection.insertOne(leaveRequest);
-
-    // return {
-    //   leaveId: result.insertedId.toString(),
-
-    //   employeeId: event.employeeId,
-    //   employeeName: event.employeeName,
-    //   leaveType: event.leaveType,
-    //   fromDate: event.fromDate,
-    //   toDate: event.toDate,
-    //   reason: event.reason,
-
-    //   status: "PENDING_MANAGER"
-    // };
+    };
 
   } catch (err) {
     console.error("Create Leave Error:", err);
